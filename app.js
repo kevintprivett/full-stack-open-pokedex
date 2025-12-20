3646046f41793a75c1e12fcf53c520d9444e514f
kevintprivett <kevintprivett@gmail.com>
2025-12-20 20:41:02 +0000

    Work on 11.12. Deployment should fail due to broken health check.

--- app.js
+++ app.js
@@ -14,3 +14,9 @@ app.listen(PORT, () => {
 app.get('/version', (req, res) => {
   res.send('1') // update string to show a new version
 })
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
+})
